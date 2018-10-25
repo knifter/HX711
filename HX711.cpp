05b549503334e09287e083581904f3d2a4bdb10f
Tijs van Roon <troon@uva.nl>
2018-10-25 16:28:59 +0200

    Knifterified: - Changed uppercase naming of class variables - Added typedef for gain

--- HX711.cpp
+++ HX711.cpp
@@ -1,56 +1,39 @@
 #include <Arduino.h>
 #include <HX711.h>
 
-#if ARDUINO_VERSION <= 106
-    // "yield" is not implemented as noop in older Arduino Core releases, so let's define it.
-    // See also: https://stackoverflow.com/questions/34497758/what-is-the-secret-of-the-arduino-yieldfunction/34498165#34498165
-    void yield(void) {};
-#endif
-
-HX711::HX711(byte dout, byte pd_sck, byte gain) {
-	begin(dout, pd_sck, gain);
+HX711::HX711(uint8_t dout, uint8_t sck, gain_t gain)
+{
+	begin(dout, sck, gain);
 }
 
-HX711::HX711() {
-}
+void HX711::begin(uint8_t dout, uint8_t sck, gain_t gain)
+{
+    _sck = sck;
+	_dout = dout;
 
-HX711::~HX711() {
-}
-
-void HX711::begin(byte dout, byte pd_sck, byte gain) {
-	PD_SCK = pd_sck;
-	DOUT = dout;
-
-	pinMode(PD_SCK, OUTPUT);
-	pinMode(DOUT, INPUT);
+	pinMode(_sck, OUTPUT);
+	pinMode(_dout, INPUT);
 
 	set_gain(gain);
 }
 
-bool HX711::is_ready() {
-	return digitalRead(DOUT) == LOW;
+bool HX711::is_ready()
+{
+	return digitalRead(_dout) == LOW;
 }
 
-void HX711::set_gain(byte gain) {
-	switch (gain) {
-		case 128:		// channel A, gain factor 128
-			GAIN = 1;
-			break;
-		case 64:		// channel A, gain factor 64
-			GAIN = 3;
-			break;
-		case 32:		// channel B, gain factor 32
-			GAIN = 2;
-			break;
-	}
-
-	digitalWrite(PD_SCK, LOW);
+void HX711::set_gain(gain_t gain)
+{
+    _gain = gain;
+	digitalWrite(_sck, LOW);
 	read();
 }
 
-long HX711::read() {
+long HX711::read()
+{
 	// wait for the chip to become ready
-	while (!is_ready()) {
+	while (!is_ready())
+    {
 		// Will do nothing on Arduino but prevent resets of ESP8266 (Watchdog Issue)
 		yield();
 	}
@@ -60,18 +43,20 @@ long HX711::read() {
 	uint8_t filler = 0x00;
 
 	// pulse the clock pin 24 times to read the data
-	data[2] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-	data[1] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-	data[0] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+	data[2] = shiftIn(_dout, _sck, MSBFIRST);
+	data[1] = shiftIn(_dout, _sck, MSBFIRST);
+	data[0] = shiftIn(_dout, _sck, MSBFIRST);
 
 	// set the channel and the gain factor for the next reading using the clock pin
-	for (unsigned int i = 0; i < GAIN; i++) {
-		digitalWrite(PD_SCK, HIGH);
-		digitalWrite(PD_SCK, LOW);
+	for (unsigned int i = 0; i < _gain; i++)
+    {
+		digitalWrite(_sck, HIGH);
+		digitalWrite(_sck, LOW);
 	}
 
 	// Replicate the most significant bit to pad out a 32-bit signed integer
-	if (data[2] & 0x80) {
+	if (data[2] & 0x80)
+    {
 		filler = 0xFF;
 	} else {
 		filler = 0x00;
@@ -86,49 +71,57 @@ long HX711::read() {
 	return static_cast<long>(value);
 }
 
-long HX711::read_average(byte times) {
+long HX711::read_average(uint8_t times)
+{
 	long sum = 0;
-	for (byte i = 0; i < times; i++) {
+	for (uint8_t i = 0; i < times; i++)
+    {
 		sum += read();
 		yield();
 	}
 	return sum / times;
 }
 
-double HX711::get_value(byte times) {
-	return read_average(times) - OFFSET;
+double HX711::get_value(uint8_t times)
+{
+	return read_average(times) - _offset;
 }
 
-float HX711::get_units(byte times) {
-	return get_value(times) / SCALE;
+float HX711::get_units(uint8_t times)
+{
+	return (read_average(times) - _offset) / _scalefactor;
 }
 
-void HX711::tare(byte times) {
-	double sum = read_average(times);
-	set_offset(sum);
+void HX711::tare(uint8_t times) {
+	_offset = read_average(times);
 }
 
-void HX711::set_scale(float scale) {
-	SCALE = scale;
+void HX711::set_scale(float scale)
+{
+	_scalefactor = scale;
 }
 
-float HX711::get_scale() {
-	return SCALE;
+float HX711::get_scale()
+{
+	return _scalefactor;
 }
 
-void HX711::set_offset(long offset) {
-	OFFSET = offset;
+void HX711::set_offset(long offset)
+{
+	_offset = _offset;
 }
 
 long HX711::get_offset() {
-	return OFFSET;
+	return _offset;
 }
 
-void HX711::power_down() {
-	digitalWrite(PD_SCK, LOW);
-	digitalWrite(PD_SCK, HIGH);
+void HX711::power_down()
+{
+	digitalWrite(_sck, LOW);
+	digitalWrite(_sck, HIGH);
 }
 
-void HX711::power_up() {
-	digitalWrite(PD_SCK, LOW);
+void HX711::power_up()
+{
+	digitalWrite(_sck, LOW);
 }
--- HX711.h
+++ HX711.h
@@ -9,25 +9,25 @@
 
 class HX711
 {
-	private:
-		byte PD_SCK;	// Power Down and Serial Clock Input Pin
-		byte DOUT;		// Serial Data Output Pin
-		byte GAIN;		// amplification factor
-		long OFFSET = 0;	// used for tare weight
-		float SCALE = 1;	// used to return weight in grams, kg, ounces, whatever
+public:
+		typedef enum
+		{
+			GAIN_128 = 1,
+			GAIN_32 = 2,
+			GAIN_64 = 3
+		} gain_t;
 
-	public:
 		// define clock and data pin, channel, and gain factor
 		// channel selection is made by passing the appropriate gain: 128 or 64 for channel A, 32 for channel B
 		// gain: 128 or 64 for channel A; channel B works with 32 gain factor only
-		HX711(byte dout, byte pd_sck, byte gain = 128);
+		HX711(uint8_t dout, uint8_t pd_sck, gain_t gain = GAIN_128);
 
-		HX711();
+		HX711() {};
 
-		virtual ~HX711();
+		virtual ~HX711() {};
 
 		// Allows to set the pins and gain later than in the constructor
-		void begin(byte dout, byte pd_sck, byte gain = 128);
+		void begin(uint8_t dout, uint8_t pd_sck, gain_t gain = GAIN_128);
 
 		// check if HX711 is ready
 		// from the datasheet: When output data is not ready for retrieval, digital output pin DOUT is high. Serial clock
@@ -37,28 +37,27 @@ class HX711
 		// set the gain factor; takes effect only after a call to read()
 		// channel A can be set for a 128 or 64 gain; channel B has a fixed 32 gain
 		// depending on the parameter, the channel is also set to either A or B
-		void set_gain(byte gain = 128);
+		void set_gain(gain_t gain = GAIN_128);
 
 		// waits for the chip to be ready and returns a reading
 		long read();
 
 		// returns an average reading; times = how many times to read
-		long read_average(byte times = 10);
+		long read_average(uint8_t times = 10);
 
 		// returns (read_average() - OFFSET), that is the current value without the tare weight; times = how many readings to do
-		double get_value(byte times = 1);
+		double get_value(uint8_t times = 1);
 
 		// returns get_value() divided by SCALE, that is the raw value divided by a value obtained via calibration
 		// times = how many readings to do
-		float get_units(byte times = 1);
+		float get_units(uint8_t times = 1);
 
 		// set the OFFSET value for tare weight; times = how many times to read the tare value
-		void tare(byte times = 10);
+		void tare(uint8_t times = 10);
 
 		// set the SCALE value; this value is used to convert the raw data to "human readable" data (measure units)
 		void set_scale(float scale = 1.f);
-
-		// get the current SCALE
+		// // get the current SCALE
 		float get_scale();
 
 		// set OFFSET, the value that's subtracted from the actual reading (tare weight)
@@ -72,6 +71,13 @@ class HX711
 
 		// wakes up the chip after power down mode
 		void power_up();
+
+	private:
+		uint8_t _sck;	// Power Down and Serial Clock Input Pin
+		uint8_t _dout;		// Serial Data Output Pin
+		uint8_t _gain;		// amplification factor
+		long _offset 		= 0;	// used for tare weight
+		float _scalefactor = 1.0;
 };
 
 #endif /* HX711_h */
